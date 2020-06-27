4ce286c2edd5e89d5b8de62d740901bc2b230f33
MoKi <61506669+moki929@users.noreply.github.com>
2020-06-26 22:04:54 -0400

    Update Euler1.py

--- Euler1.py
+++ Euler1.py
@@ -1,3 +1,4 @@
+#This is like Fizz Buzz. I remember seeing this to 100, and in JavaScript. Cool!!
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
 # The sum of these multiples is 23.
 
